433d899d1e0c52140cea82a419e1e5c8c2da2498
gitKashish <125540624+gitKashish@users.noreply.github.com>
2024-01-18 19:44:32 +0530

    Added Error Handling for File does not exists
    However error handling does not seem to be optimal

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,31 @@ extern crate flate2;
 
 use flate2::{write::GzEncoder,
             Compression};
-use std::{io::{copy, BufReader},
-        env::args,
+use std::{env::args,
         fs::File,
         time::Instant};
+use std::io::{copy, BufReader};
 
 fn main() {
     if args().len() != 3 {
         eprintln!("Usage: `source` `target`");
         return;
     }
+    let source_path = args().nth(1).unwrap();
+    let target_path = args().nth(2).unwrap();
+    
+    let source_file = File::open(source_path);
+    let source_file = match source_file {
+        Ok(file) => file,
+        Err(_) => {
+            eprintln!("Error: Problem opening the source file");
+            return;
+        }
+    };
 
-    let mut input = BufReader::new(
-            File::open(args().nth(1).unwrap()).unwrap()
-            );
-    let output = File::create(args().nth(2).unwrap()).unwrap();
+    let mut input = BufReader::new(source_file);
+
+    let output = File::create(target_path).unwrap();
     let mut encoder = GzEncoder::new(output, Compression::default());
     let start = Instant::now();
 
